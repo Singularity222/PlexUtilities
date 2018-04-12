4ff9a691bb25823179c9d425ee01992a4e29794b
bill.applegate <bill.applegate@ubc.com>
2018-04-12 16:40:38 -0400

    Initial commit.

---
+++ PlexUtilities/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace PlexUtilities
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            const string BasePath = @"\\typhon\f\Cleaned Movies\";
+            string[] directories = Directory.GetDirectories(BasePath);
+
+            foreach (string directory in directories)
+            {
+                if (!ContainsPosterImage(directory))
+                {
+                    Console.WriteLine(directory);
+                }
+            }
+        }
+
+        public static bool ContainsPosterImage(string directory)
+        {
+            string[] files = Directory.GetFiles(directory);
+
+            return files.Any(file => file.Contains("poster"));
+        }
+    }
+}
